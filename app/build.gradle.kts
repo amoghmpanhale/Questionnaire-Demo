84a50834aa42c69fb16668db22b5f6402a8e9c91
Amogh Panhale <amoghmpanhale@gmail.com>
2026-02-09 18:27:05 -0700

    Updated the app with a dashboard thingy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.questionnaire_demo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.questionnaire_demo"
